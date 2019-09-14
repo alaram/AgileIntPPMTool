dd7931bdb239ae21701f1315a603299a4caa5f62
alaram <zippisan78@gmail.com>
2019-09-14 13:13:59 +0200

    First commit

---
+++ src/test/java/se/msc/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package se.msc.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
